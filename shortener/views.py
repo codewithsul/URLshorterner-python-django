4d8ccbebf6be8b8d530c46ef5c9e101cfb9a8748
Suleiman123 <suleiman4a@gmail.com>
2022-09-02 19:46:22 -0700

    making changes

--- shortener/views.py
+++ shortener/views.py
@@ -15,7 +15,6 @@ def create(request):
     if request.method == 'POST':
         link = request.POST['link']
         uid = str(uuid.uuid4())[:5]
-        
         new_url = URL(link=link, uuid= uid)
         new_url.save()
         return HttpResponse(uid)
--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 import django_heroku
 import dj_database_url
 
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'urlshortener.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS':[],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,9 +118,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+django_heroku.settings(locals())
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
