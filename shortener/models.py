039186853e017d7bfaf7a0510428dfa9a05274f9
Suleiman123 <suleiman4a@gmail.com>
2022-09-03 09:37:52 -0700

    database changes

--- shortener/models.py
+++ shortener/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 class URL(models.Model):
-    link = models.CharField(max_length=64)
+    link = models.CharField(max_length=10000)
     uuid = models.CharField(max_length=10)
--- shortener/urls.py
+++ shortener/urls.py
@@ -8,5 +8,5 @@ from . import views
 urlpatterns = [
     path('',views.index, name="index"),
     path('create',views.create, name="create"),
-    path('<str:pk>',views.go, name="redirect")
+    path('<str:pk>',views.go, name="go")
 ]
